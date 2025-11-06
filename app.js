cb24b28b504e27b61d0fbb2d2b4fa4047256e2ac
jhasaurav97 <jhasaurav593@gmail.com>
2025-11-06 14:10:51 +0530

    Added root route for live Render check

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use("/api/admin", adminRoutes);
 // Error handler
 app.use(errorHandler);
 
+app.get("/", (req, res) => {
+    res.send("Pixisphere Backend API is running successfully!")
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 });
